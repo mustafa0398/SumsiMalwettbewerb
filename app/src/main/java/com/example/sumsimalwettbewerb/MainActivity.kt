acdaee918f2f58cc37d6ebf28d42fdc12a94f3ab
Mustafa Öztürk <Mustafa.Oeztuerk@edu.fh-kaernten.ac.at>
2023-12-21 13:05:34 +0100

    Startseite-Layouts

--- app/src/main/java/com/example/sumsimalwettbewerb/MainActivity.kt
+++ app/src/main/java/com/example/sumsimalwettbewerb/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.sumsimalwettbewerb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // NavigationBar
+        val bottomNavView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        bottomNavView.setOnNavigationItemSelectedListener { item ->
+            true
+        }
+
+
     }
 }
