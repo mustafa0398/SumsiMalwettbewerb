16b4be9c44cdf71bfc2b7f1f6a5a91a170d9157f
Mustafa Öztürk <Mustafa.Oeztuerk@edu.fh-kaernten.ac.at>
2023-12-22 13:03:00 +0100

    Fragments and two Screens

--- app/src/main/java/com/example/sumsimalwettbewerb/MainActivity.kt
+++ app/src/main/java/com/example/sumsimalwettbewerb/MainActivity.kt
@@ -1,25 +1,29 @@
 package com.example.sumsimalwettbewerb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.sumsimalwettbewerb.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Toolbar
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        // NavigationBar
-        val bottomNavView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
-        bottomNavView.setOnNavigationItemSelectedListener { item ->
-            true
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
-
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
